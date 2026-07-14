904f02d71232666e73ad20114553d7e6128c97b4
Artem Pak <artemepak@gmail.com>
2026-07-14 22:33:17 +0700

    finish homework-4

--- to-do-list/src/app/components/to-do-item/to-do-item.ts
+++ to-do-list/src/app/components/to-do-item/to-do-item.ts
@@ -1,13 +1,15 @@
 import { Component, input, output } from '@angular/core';
 import { ToDoButton } from '../to-do-button/to-do-button';
+import { TooltipDirective } from '../../directives/tooltip';
 
 @Component({
   selector: 'app-to-do-item',
-  imports: [ToDoButton],
+  imports: [ToDoButton, TooltipDirective],
   templateUrl: './to-do-item.html',
   styleUrl: './to-do-item.css',
 })
 export class ToDoItem {
   text = input.required<string>();
   delete = output<void>();
+  isSelected = input<boolean>(false);
 }
--- to-do-list/src/app/components/to-do-list/to-do-list.ts
+++ to-do-list/src/app/components/to-do-list/to-do-list.ts
@@ -1,14 +1,16 @@
-import { Component, computed, OnInit, signal } from '@angular/core';
+import { Component, computed, OnInit, signal, ChangeDetectionStrategy } from '@angular/core';
 import { ToDoHeader } from '../to-do-header/to-do-header';
 import { FormsModule } from '@angular/forms';
 import { ToDoItem } from '../to-do-item/to-do-item';
 import { MatInputModule } from '@angular/material/input';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { ToDoButton } from '../to-do-button/to-do-button';
+import { TooltipDirective } from '../../directives/tooltip';
 
 interface Task {
   id: number;
   text: string;
+  description: string;
 }
 
 @Component({
@@ -20,17 +22,30 @@ interface Task {
     MatInputModule,
     MatProgressSpinnerModule,
     ToDoButton,
+    TooltipDirective,
   ],
   templateUrl: './to-do-list.html',
   styleUrl: './to-do-list.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ToDoList implements OnInit {
   isLoading = signal<boolean>(true);
   inputValue = signal('');
+  textareaValue = signal('');
+  selectedItemId = signal<number | null>(null);
+
   tasks = signal<Task[]>([
-    { id: 1, text: 'Task 1' },
-    { id: 2, text: 'Task 2' },
+    { id: 1, text: 'Task 1', description: 'description 1' },
+    { id: 2, text: 'Task 2', description: 'description 2' },
   ]);
+
+  selectedDescriptionTask = computed(() => {
+    const selectedId = this.selectedItemId();
+    if (selectedId === null) return;
+
+    const task = this.tasks().find(task => task.id === selectedId);
+    return task ? task.description : 'Задача не найдена';
+  });
 
   ngOnInit() {
     setTimeout(() => {
@@ -51,7 +66,9 @@ export class ToDoList implements OnInit {
     const maxId = Math.max(...currentTasks.map((task) => task.id), 0);
     const id = maxId + 1;
     const text = this.inputValue().trim();
-    this.tasks.update((tasks) => [...tasks, { id, text }]);
+    const description = this.textareaValue().trim();
+    this.tasks.update((tasks) => [...tasks, { id, text, description }]);
     this.inputValue.set('');
+    this.textareaValue.set('');
   }
 }
